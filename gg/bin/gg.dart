7337e66331ab4f41f25792aa64e4cbfa6c3d9c53
joshua <josuethetika01@gmail.com>
2026-04-08 00:48:55 +0000

    newChanges

---
+++ gg/bin/gg.dart
@@ -0,0 +1,5 @@
+import 'package:gg/gg.dart' as gg;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gg.calculate()}!');
+}
---
+++ gg/test/gg_test.dart
@@ -0,0 +1,8 @@
+import 'package:gg/gg.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
