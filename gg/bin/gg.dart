be21f7b6ee1c6643833da9a00a512d0c52e75098
joshua <josuethetika01@gmail.com>
2026-04-08 00:50:36 +0000

    newChanges

--- gg/bin/gg.dart
+++
@@ -1,5 +0,0 @@
-import 'package:gg/gg.dart' as gg;
-
-void main(List<String> arguments) {
-  print('Hello world: ${gg.calculate()}!');
-}
--- gg/test/gg_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:gg/gg.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
